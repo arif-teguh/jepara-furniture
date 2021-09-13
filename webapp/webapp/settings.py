76273196ff1fe7ff3887b557ee73fae34d1ad4fd
arif-teguh <arifteguhwangi@gmail.com>
2021-09-13 21:14:03 +0700

    edit setting.py for database mysql

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jepara_furniture',
+        'USER': 'mysql',
+        'PASSWORD': 'arif1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
